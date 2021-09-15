b8e75094b1c0a0d146b0384d140cf372a7f0c4d4
hmcho <hahahihiho16@gmail.com>
2021-09-15 17:49:18 +0900

    chmod .sh and hardhat config

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,8 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 const fs = require('fs');
-const privateKeys = fs.readFileSync("../.secret/.test_priv_keys").toString().split('\r\n').map(k=>'0x'+k) || ["8dda6cdcfdbf6ea764231841def05c64cf57ed0df9cc92cfca3f2c2997690891"]
+const privateKeys = fs.readFileSync("../.secret/.test_priv_keys").toString().split('\n').map(k=>'0x'+k) || ["8dda6cdcfdbf6ea764231841def05c64cf57ed0df9cc92cfca3f2c2997690891"]
 const accounts = [];
-privateKeys.forEach(k => accounts.push({privateKey:k,balance:'1000'}))
+privateKeys.forEach(k => accounts.push({privateKey:k,balance:String(100*10**18)}))
 
 module.exports = {
   defaultNetwork: "hardhat",

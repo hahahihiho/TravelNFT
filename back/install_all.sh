ad83f5282517ee7e7fa9a51604ea4e8b8d721cbe
hmcho-multipass <hahahihiho16@gmail.com>
2021-09-16 18:37:42 +0900

    make integrated .sh

---
+++ back/install_all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./install_docker.sh
+./install_nodejs.sh
+./install_server_ipfs.sh
+./install_hardhat.sh
---
+++ back/run_all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./run_ipfs.sh
+./run_local_hardhat.sh
+./compile_contracts.sh
+./deploy_contract_local.sh

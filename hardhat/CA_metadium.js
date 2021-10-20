1e7c656c88f87a1d0cb87cce71877e0baef4ba31
NT551 <hi@hi.com>
2021-10-20 11:34:21 +0900

    update hardhat

--- hardhat/CA_metadium.js
+++ hardhat/CA_metadium.js
@@ -1,5 +1,5 @@
 
-  const nftmarketaddress = "0xcD83455dE4812A037aDFc3148c1305ABcA4Dc8c3"
-  const nftaddress = "0xf67d21eA44769e3AEe08cAd42856dd56e45d0715"
+  const nftmarketaddress = "0x1a7364Ab04034E39F1c0a4F0F152F01d92B48190"
+  const nftaddress = "0x44266378791f9b1938d513B2CA5674eC21418623"
   module.exports = {nftmarketaddress, nftaddress}
   
